df68338ab9e50abd4c3eff45e1256e1d1dc0ce63
weierstrass1 <pndiaz1@uc.cl>
2018-07-14 17:18:20 -0400

    Dyzen aef

---
+++ Dyzen/Dyzen.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Dyzen
+{
+    partial class Dyzen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dyzen));
+            this.gfxBox1 = new controls.GFXBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gfxBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gfxBox1
+            // 
+            this.gfxBox1.BehindBitmap = ((System.Drawing.Bitmap)(resources.GetObject("gfxBox1.BehindBitmap")));
+            this.gfxBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.gfxBox1.Image = ((System.Drawing.Image)(resources.GetObject("gfxBox1.Image")));
+            this.gfxBox1.Location = new System.Drawing.Point(114, 79);
+            this.gfxBox1.Name = "gfxBox1";
+            this.gfxBox1.Selection = new System.Drawing.Rectangle(0, 0, 64, 64);
+            this.gfxBox1.SelectionAccuracy = 8;
+            this.gfxBox1.SelectionMinSize = 16;
+            this.gfxBox1.Size = new System.Drawing.Size(512, 256);
+            this.gfxBox1.TabIndex = 0;
+            this.gfxBox1.TabStop = false;
+            this.gfxBox1.Zoom = 4;
+            // 
+            // Dyzen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gfxBox1);
+            this.Name = "Dyzen";
+            this.Text = "Dyzen";
+            ((System.ComponentModel.ISupportInitialize)(this.gfxBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private controls.GFXBox gfxBox1;
+    }
+}
---
+++ Dyzen/Dyzen.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using backend;
+using System.IO;
+using System;
+
+namespace Dyzen
+{
+    public partial class Dyzen : Form
+    {
+        private static Zoom zoom = Zoom.x4;
+
+        public static Zoom Zoom
+        {
+            set
+            {
+                if(zoom!=value)
+                {
+                    zoom = value;
+                    ZoomChanged?.Invoke();
+                }
+            }
+        }
+
+        public static int IntZoom { get { return (int)zoom; } }
+
+        public static event Action ZoomChanged;
+        public Dyzen()
+        {
+            InitializeComponent();
+
+            ZoomChanged += Dyzen_ZoomChanged;
+
+            ColorPalette.GeneratePalette("Doom3.pal", 16);
+
+            gfxBox1.GetTiles("Doom3.bin", TileSize.Size16x16, BaseTile.Top);
+        }
+
+        private void Dyzen_ZoomChanged()
+        {
+            gfxBox1.Zoom = IntZoom;
+        }
+    }
+}
